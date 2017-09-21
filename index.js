1e68a735fa1cc77a2f723850323053367fd5c3a3
Tyler Bristol <tahbristol@gmail.com>
2017-09-21 02:40:21 +0000

    createRecipe: got form data

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+
+  var name = document.getElementById('name').value;
+  var description = document.getElementById('description').value;
+  var in1 = document.getElementById('ingredient_1').value;
+  var in2 = document.getElementById('ingredient_2').value;
+  var in3 = document.getElementById('ingredient_3').value;
+  var in4 = document.getElementById('ingredient_4').value;
+  var in5 = document.getElementById('ingredient_5').value;
+
+  var recipe = {
+
+    'name': name,
+    'description': description,
+    'ingredient_1': in1,
+    'ingredient_2': in2,
+    'ingredient_3': in3,
+    'ingredient_4': in4,
+    'ingredient_5': in5
+  }
+
+
+
+  console.log(recipe);
+}
