6555644ce496df9580767b1cfe35802868cede00
Tyler Bristol <tahbristol@gmail.com>
2017-09-28 16:26:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById('recipe-form-partial').innerHTML);
+  Handlebars.registerHelper('displayIngredient', function(){
+
+
+         return new Handlebars.SafeString("<li>" + this.name + "</li>");
+
+  });
+  var recipe = {
+
+    name: "Food",
+    description: "food description",
+    ingredients: [
+      "ing1", "ing2", "ing3", "ing4", "ing5"
+    ]
+  }
+
+
+  var recipeTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+
+  var recipeResult = recipeTemplate(recipe);
+  console.log(recipeResult);
+  var main = document.getElementById('main');
+  main.innerHTML = recipeResult;
+
+
+
+
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
-})
+});
+
+function displayEditForm(){
+  var recipe = {
+
+    name: "Food",
+    description: "food description",
+    ingredients: [
+      "ing1", "ing2", "ing3", "ing4", "ing5"
+    ]
+  }
+
+
+  var recipeTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+
+  var recipeResult = recipeTemplate(recipe);
+  console.log(recipeResult);
+  var main = document.getElementById('main');
+  main.innerHTML = recipeResult;
+
+
+
+}
+
 
 function createRecipe() {
 
@@ -15,18 +67,46 @@ function createRecipe() {
   var in4 = document.getElementById('ingredient_4').value;
   var in5 = document.getElementById('ingredient_5').value;
 
-  var recipe = {
+  var newRecipe = {
+    name: name,
+    description: description,
+    ingredients: [{
+        amount: "one",
+        name: in1
+      },
+      {
+        amount: "two",
+        name: in2
+      },
+      {
+        amount: "three",
+        name: in3
+      },
+      {
+        amount: "four",
+        name: in4
+      },
+      {
+        amount: "five",
+        name: in5
+      }
 
-    'name': name,
-    'description': description,
-    'ingredient_1': in1,
-    'ingredient_2': in2,
-    'ingredient_3': in3,
-    'ingredient_4': in4,
-    'ingredient_5': in5
+    ]
   }
 
 
 
-  console.log(recipe);
+
+
+  console.log(newRecipe);
+  var recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  console.log("Here is the template fxn: " + recipeTemplate);
+  var result = recipeTemplate(newRecipe);
+  var recipesDiv = document.getElementById('recipes');
+  recipesDiv.innerHTML += recipeTemplate(newRecipe);
+}
+
+function updateRecipe(){
+
+  createRecipe();
 }
