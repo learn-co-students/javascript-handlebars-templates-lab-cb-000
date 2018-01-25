1d8df31d1857208f0609102b4ef401c1c58b396c
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-25 20:20:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
+  //need to register a few partials and things? 
 }
+
+function createRecipe() {
+
+}
+
+function displayEditForm() {
+//Run this when edit recipe is clicked
+//Render recipe-form-template with updateRecipe as the submit function
+//Make sure the existing data shows up if it's in edit mode
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
