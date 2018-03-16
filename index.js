97322517fc5cd48ce37e8d41497acc6692786a7d
Heather Christman <efl7a@yahoo.de>
2018-03-16 18:34:27 -0400

    Working on number 3

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var recipe = {
+    description: 'yummy chicken noodle soup',
+    ingredients: [
+      {quantity: "1 cup", name: 'chicken'},
+      {quantity: "3 nanoliters", name: 'stock'},
+      {quantity: "12", name: 'noodles'}
+    ]
+  }
+
+  var template = Handlebars.compile(document.getElementById("my-template").innerHTML);
+  var html = template(recipe);
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
